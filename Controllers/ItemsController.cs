05ecd03543fc8c9ce5c8c4198328200a85c9c95a
Anik Ghosh <ldexter0910@gmail.com>
2022-02-04 03:16:37 +0530

    Initial web api practise using dotnet

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+using Catalog.Repositories;
+using Catalog.Entities;
+
+namespace Catalog.Controllers;
+
+[ApiController]
+[Route("items")]
+public class ItemsController: ControllerBase {
+
+    private readonly InMemItemsRepo repo;
+    ItemsController() {
+        this.repo = new InMemItemsRepo();
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Item>> GetItems(){
+        return Ok(this.repo.GetItems());
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Item> GetItem(Guid id) {
+        Item item = this.repo.GetItem(id);
+
+        if (item is null) {
+            return NotFound();
+        }
+
+        return Ok(this.repo.GetItem(id));
+    }
+}
---
+++ Repositories/InMemItemsRepo.cs
@@ -0,0 +1,19 @@
+using Catalog.Entities;
+
+namespace Catalog.Repositories;
+public class InMemItemsRepo {
+    public readonly List<Item> items = new()
+    {
+        new Item() { Id = Guid.NewGuid(), Name="Potion", Price=9, CreatedDate = DateTimeOffset.UtcNow },
+        new Item() { Id = Guid.NewGuid(), Name="Iron Sword", Price=20, CreatedDate = DateTimeOffset.UtcNow },
+        new Item() { Id = Guid.NewGuid(), Name="Bronze Shield", Price=18, CreatedDate = DateTimeOffset.UtcNow }
+    };
+
+    public IEnumerable<Item> GetItems() {
+        return this.items;
+    }
+
+    public Item GetItem(Guid id) {
+        return this.items.Where(item => item.Id == id).SingleOrDefault();
+    }
+}
